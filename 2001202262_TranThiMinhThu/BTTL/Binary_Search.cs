663bda5aa06798176facbed24a756b7f218762b2
tranthiminhthu <thuminhtran147002@gmail.com>
2024-07-22 20:23:31 +0700

    first commit

---
+++ 2001202262_TranThiMinhThu/BTTL/Binary_Search.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTTL
+{
+    internal class Binary_Search
+    {
+        public static int Search(int[] A, int n, int key)
+        {
+            int left = 0;
+            int right = n - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (A[mid] == key)
+                    return mid;
+                if (key < A[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] A = { 1, 3, 5, 7, 9 };
+            int n = A.Length;
+            int key = 5;
+
+            int result = Binary_Search.Search(A, n, key);
+            Console.WriteLine(result);
+        }
+    }
+}
